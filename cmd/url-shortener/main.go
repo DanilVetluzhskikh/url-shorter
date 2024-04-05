1ca82599b9c892d9cfb4743a40ee998382f01677
DanilVetluzhskikh <vetlzdanilvv@gmail.com>
2024-04-05 19:46:14 +0500

    storage & logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"learn/internal/config"
+	sl "learn/internal/lib/logger"
+	"learn/internal/logger"
+	"learn/internal/storage/sqlite"
+	"log/slog"
+	"os"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	logger := logger.SetupLogger(cfg.Env)
 
-	fmt.Println(cfg)
+	logger.Info("url-shortener started", slog.String("env", cfg.Env))
+	logger.Info("debug has started")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+
+	if err != nil {
+		logger.Error("failed init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	if err != nil {
+		logger.Error("failed init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 }
